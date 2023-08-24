04a31ef446448cd5afa19bb3b287dbdbb84e20dc
Karthik D K <karthikdk1998@gmail.com>
2023-08-25 01:13:23 +0530

    motors test

--- dust/src/main.rs
+++ dust/src/main.rs
@@ -4,8 +4,8 @@ use anyhow::Result;
 use rppal::gpio::Level;
 
 use drishti::depth::Ultrasonic;
-use vahana::drive::Motor;
 use vahana::drive::init_i2c;
+use vahana::drive::Motor;
 
 fn main() -> Result<()> {
     // let image_path = "images/image.jpg";
@@ -23,24 +23,27 @@ fn main() -> Result<()> {
     motor.right_rear_pwm_pin.prescaler(10)?;
 
     println!("MOTORS STARTED.......................................");
-    for iter in 0..10 {
-        for i in (0..4095).step_by(10) {
-            motor.left_rear_dir_pin.write(Level::High);
-            let _ = motor.left_rear_pwm_pin.pulse_width(i);
-            motor.right_rear_dir_pin.write(Level::Low);
-            let _ = motor.right_rear_pwm_pin.pulse_width(i);
-        }
-        thread::sleep(Duration::from_secs(2));
-        println!("***************************************************");
-        for i in (0..4095).rev().step_by(10) {
-            motor.left_rear_dir_pin.write(Level::Low);
-            let _ = motor.left_rear_pwm_pin.pulse_width(i);
-            motor.right_rear_dir_pin.write(Level::High);
-            let _ = motor.right_rear_pwm_pin.pulse_width(i);
-        }
-        thread::sleep(Duration::from_secs(2));
-        println!("ITERATION: {iter}");
-    }
+
+    motor.left_rear_dir_pin.write(Level::High);
+    let _ = motor.left_rear_pwm_pin.pulse_width(1000);
+    motor.right_rear_dir_pin.write(Level::Low);
+    let _ = motor.right_rear_pwm_pin.pulse_width(1000);
+    thread::sleep(Duration::from_secs(2));
+
+    println!("***************************************************");
+    motor.left_rear_dir_pin.write(Level::Low);
+    let _ = motor.left_rear_pwm_pin.pulse_width(0);
+    motor.right_rear_dir_pin.write(Level::High);
+    let _ = motor.right_rear_pwm_pin.pulse_width(0);
+    thread::sleep(Duration::from_secs(2));
+
+    println!("***************************************************");
+    motor.left_rear_dir_pin.write(Level::High);
+    let _ = motor.left_rear_pwm_pin.pulse_width(500);
+    motor.right_rear_dir_pin.write(Level::Low);
+    let _ = motor.right_rear_pwm_pin.pulse_width(500);
+    thread::sleep(Duration::from_secs(2));
+
     println!("MOTORS STOPPED.......................................");
 
     let trig_pin = 27; // D2 (robot-hat)
