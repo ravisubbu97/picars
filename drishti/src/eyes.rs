9095350a915e3bcd38ccb73179662cea9c082d57
Karthik D K <karthikdk1998@gmail.com>
2023-09-06 10:24:34 +0530

    add canny algo

--- drishti/src/eyes.rs
+++ drishti/src/eyes.rs
@@ -3,14 +3,18 @@ use std::process::Command;
 use anyhow::{Context, Result};
 use opencv::highgui;
 use opencv::imgcodecs;
+use opencv::imgproc;
 use opencv::prelude::*;
 
 pub fn cv_example(img_path: &str) -> Result<()> {
     let image =
-        imgcodecs::imread(img_path, imgcodecs::IMREAD_COLOR).context("Image reading failed")?;
+        imgcodecs::imread(img_path, imgcodecs::IMREAD_GRAYSCALE).context("Image reading failed")?;
+    let mut edges = Mat::default();
 
-    highgui::imshow("Display window", &image).context("Image display failed")?;
-    highgui::wait_key(0).context("Waiting for a keystroke in the window failed")?; // Wait for a keystroke in the window
+    imgproc::canny(&image, &mut edges, 50.0, 150.0, 3, false).context("Canny algo failed")?;
+    highgui::imshow("Original Image", &image).context("Image show failed")?;
+    highgui::imshow("Canny Edges", &edges).context("Image show failed")?;
+    highgui::wait_key(0).context("Waiting for a keystroke in the window failed")?;
 
     Ok(())
 }
