38395d91c1d811891cf5225e1e4d1ed36545b193
Karthik D K <karthikdk1998@gmail.com>
2023-09-23 16:39:30 +0530

    save output images from opencv

--- drishti/src/eyes.rs
+++ drishti/src/eyes.rs
@@ -1,27 +1,31 @@
-use std::process::Command;
+use std::{
+    io::Error,
+    process::{Command, Output},
+    thread,
+    time::Duration,
+};
 
 use anyhow::{Context, Result};
-use opencv::highgui;
-use opencv::imgcodecs;
-use opencv::imgproc;
-use opencv::prelude::*;
+use opencv::{core, imgcodecs, imgproc, prelude::*};
 
-pub fn cv_example(img_path: &str) -> Result<()> {
+pub fn cv_example(img_path: &str, cap_img_path: &str, edge_img_path: &str) -> Result<()> {
+    capture("1000", img_path).context("Image capture failed")?;
+    thread::sleep(Duration::from_secs(1));
+
     let image =
         imgcodecs::imread(img_path, imgcodecs::IMREAD_GRAYSCALE).context("Image reading failed")?;
     let mut edges = Mat::default();
+    let params = core::Vector::new();
 
     imgproc::canny(&image, &mut edges, 50.0, 150.0, 3, false).context("Canny algo failed")?;
-    highgui::imshow("Original Image", &image).context("Image show failed")?;
-    highgui::imshow("Canny Edges", &edges).context("Image show failed")?;
-    highgui::wait_key(0).context("Waiting for a keystroke in the window failed")?;
+    imgcodecs::imwrite(cap_img_path, &image, &params).context("Image saving failed")?;
+    imgcodecs::imwrite(edge_img_path, &edges, &params).context("Image saving failed")?;
 
     Ok(())
 }
 
-pub fn capture(timeout: &str, path: &str) {
+pub fn capture(timeout: &str, path: &str) -> Result<Output, Error> {
     Command::new("libcamera-still")
         .args(["-t", timeout, "-o", path])
         .output()
-        .expect("libcamera-still sucks !!");
 }
--- dust/src/main.rs
+++ dust/src/main.rs
@@ -2,10 +2,7 @@ use std::{thread, time::Duration};
 
 use anyhow::{Context, Result};
 
-use drishti::{
-    depth::Ultrasonic,
-    eyes::{capture, cv_example},
-};
+use drishti::{depth::Ultrasonic, eyes::cv_example};
 use vahana::{
     drive::{Motors, Servo},
     init_i2c,
@@ -17,19 +14,17 @@ fn main() -> Result<()> {
     println!("MCU RESET SUCCESSFULLY WITH PIN [{rst_pin}] [BEGIN]");
     let _i2c = init_i2c().expect("I2C INITIALIZATION FAILED");
 
-    let image_path = "image.jpg";
-    capture("1000", image_path);
-    thread::sleep(Duration::from_secs(1));
-    cv_example(image_path)?;
-
     // servo
     let mut camera_servo_pin1 = Servo::new(0).context("camera_servo_pin1 init failed")?; // P0
     let mut camera_servo_pin2 = Servo::new(1).context("camera_servo_pin2 init failed")?; // P1
     let mut dir_servo_pin = Servo::new(2).context("dir_servo_pin init failed")?; // P2
 
-    camera_servo_pin1.angle(20)?;
-    camera_servo_pin2.angle(-20)?;
-    dir_servo_pin.angle(10)?;
+    camera_servo_pin1.angle(90)?;
+    camera_servo_pin2.angle(90)?;
+    dir_servo_pin.angle(45)?;
+
+    // opencv camera example
+    cv_example("image.jpg", "captured_image.jpg", "edge_image.jpg")?;
 
     // motors
     let mut motors = Motors::new().context("motors init failed")?;
