4d085b9e38a65be048eea10224e0ba3a69d343d8
Karthik D K <karthikdk1998@gmail.com>
2023-10-25 15:21:43 +0530

    add ultrasonic example, which should run from rust

--- python/ultrasonic.py
+++ python/ultrasonic.py
@@ -1,2 +1,19 @@
-import rustimport.import_hook
-import depth
+import time
+
+import rustimport.import_hook  # noqa: F401
+
+import dust
+
+
+# Ultrasonic example check
+def us_check():
+    for _ in range(5):
+        us = dust.ultrasonic_init()
+        distance = us.read()
+        print(f"Distance: {distance} cm")
+        # Sleep for 60 milliseconds (as per DATASHEET) --> FIX ME: consider ultrasonic.read() timing into account
+        time.sleep(0.06)
+
+
+dust.main_init()
+us_check()
