a1a8b4f7a914de8b650a353f9bcfa41f6f60a8d7
Karthik D K <karthikdk1998@gmail.com>
2023-10-27 08:34:52 +0530

    use ruspy instead of dust

--- python/ultrasonic.py
+++ python/ultrasonic.py
@@ -2,18 +2,18 @@ import time
 
 import rustimport.import_hook  # noqa: F401
 
-import dust
+import ruspy
 
 
 # Ultrasonic example check
 def us_check():
+    us = ruspy.ultrasonic_init()
     for _ in range(5):
-        us = dust.ultrasonic_init()
         distance = us.read()
         print(f"Distance: {distance} cm")
         # Sleep for 60 milliseconds (as per DATASHEET) --> FIX ME: consider ultrasonic.read() timing into account
         time.sleep(0.06)
 
 
-dust.main_init()
+ruspy.main_init()
 us_check()
