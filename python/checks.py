228fd8171df757f255194097ccaa60b678e5e578
Karthik D K <karthikdk1998@gmail.com>
2023-10-27 09:26:19 +0530

    add checks for motors and servos

--- python/checks.py
+++ python/checks.py
@@ -15,5 +15,27 @@ def us_check():
         time.sleep(0.06)
 
 
+# Motors example check
+def motors_chcek():
+    motors = ruspy.motors_init()
+    motors.forward(10)
+    motors.turn_left(5)
+    motors.turn_right(15)
+    motors.backward(20)
+    motors.stop()
+
+
+# Servos example check
+def servos_check():
+    camera_servo_pin1, camera_servo_pin2, dir_servo_pin = ruspy.servos_init(
+        [10, 20, 30]
+    )
+    camera_servo_pin1.angle(90)
+    camera_servo_pin2.angle(90)
+    dir_servo_pin.angle(90)
+
+
 ruspy.main_init()
 us_check()
+motors_chcek()
+servos_check()
