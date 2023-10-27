1c55045b15b316547fcdcc791ce3aa92cd848158
Karthik D K <karthikdk1998@gmail.com>
2023-10-27 19:46:55 +0530

    adapt no lines cond

--- python/lane_follower.py
+++ python/lane_follower.py
@@ -14,9 +14,12 @@ maxLineGap = 10
 
 def run_robot(secs=10):
     started = time.time()
+    print("VIDEO CAPTURE STARTED")
     vid_cap = create_video_capture(640, 480, 30)
     motors = ruspy.motors_init(50, 100)
     motors.speed(10, 10)
+    motors.forward(10)
+    time.sleep(1)
 
     while (time.time() - started) < secs:
         for ret, frame in vid_cap.read():
@@ -28,22 +31,26 @@ def run_robot(secs=10):
             lines = cv2.HoughLinesP(
                 edged, 1, np.pi / 180, 10, minLineLength, maxLineGap
             )
-            for x in range(0, len(lines)):
-                for x1, y1, x2, y2 in lines[x]:
-                    cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    theta += math.atan2((y2 - y1), (x2 - x1))
-                    print(theta)
 
-            threshold = 6
-            if theta > threshold:
-                print("LEFT")
-                motors.turn_left(5)
-            if theta < -threshold:
-                print("RIGHT")
-                motors.turn_right(15)
-            if abs(theta) < threshold:
-                print("STRAIGHT")
-                motors.forward(10)
+            if lines is None:
+                print("NO LINES DETECTED")
+            else:
+                for x in range(0, len(lines)):
+                    for x1, y1, x2, y2 in lines[x]:
+                        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        theta += math.atan2((y2 - y1), (x2 - x1))
+                        print(theta)
+
+                threshold = 6
+                if theta > threshold:
+                    print("LEFT")
+                    motors.turn_left(5)
+                if theta < -threshold:
+                    print("RIGHT")
+                    motors.turn_right(15)
+                if abs(theta) < threshold:
+                    print("STRAIGHT")
+                    motors.forward(10)
 
             theta = 0
 
